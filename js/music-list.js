c5ccc6d2df858fefff6477cfe037f685d0a54375
Nataap01 <159050344+Nataap01@users.noreply.github.com>
2024-07-24 08:05:05 -0500

    commit initial

---
+++ js/music-list.js
@@ -0,0 +1,15 @@
+// Array de objetos que contiene la información de cada canción
+let allMusic = [
+    {
+        name: "Te regalo en corazón",      // Nombre de la canción
+        artist: "El gran Combo",           // Artista
+        img: "music-0",                    // Nombre de la imagen asociada a la canción (sin extensión)
+        src: "music0"                      // Nombre del archivo de audio (sin extensión)
+    },
+    {
+        name: "Oe bebé",
+        artist: "Blessd x Maluma",
+        img: "music-7",
+        src: "music7"
+    },
+];
